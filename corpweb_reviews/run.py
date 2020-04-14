7d987dfb659a4fe13b67756d4ac7ab824d9edb6b
canibal <canaan@thetomato.co>
2020-04-14 01:23:00 -0400

    "run.py" has one function that returns a dictionary of dictionaries, comprised of the contents of "data/feedback/" and enumerated, starting at 0

--- corpweb_reviews/run.py
+++ corpweb_reviews/run.py
@@ -3,3 +3,27 @@
 import os
 import requests
 import json
+
+data_dir = ('data/feedback/')
+file_list = os.listdir(data_dir)
+
+def create_dicts(files):
+    feedback_d = {}
+    fd = []
+    for f in files:
+        feedback = os.path.join(data_dir, f)
+        with open(feedback, 'r') as fi:
+            key_list = ['title', 'name', 'date', 'feedback']
+            val_list = []
+            for line in fi.readlines():
+                val_list.append(line.strip())
+            z = zip(key_list, val_list)
+            f_d = dict(z)
+            fd.append(f_d)
+        feedback_d = dict(list(enumerate(fd)))
+    return feedback_d
+
+
+
+if __name__=="__main__":
+    print(create_dicts(file_list))
