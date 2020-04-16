61de56c4e6f61582ad66e5f1c88904c7eac4821a
canibal <canaan@thetomato.co>
2020-04-16 02:38:26 -0400

    finishes setting up files

---
+++ final/emails.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import smtp #(et al.)
+
+def generate_email():
+    pass
+
+def send_email():
+    pass
---
+++ final/report_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+import datetime
+import reports
+
+
+
+if __name__ == "__main__"
+    reports.generate_report(attachment, title, paragraph))
+    send_email(generate_email(x, x, x, x))
