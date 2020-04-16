06bad703785206ee9f3c4c8dd30c84250e3e11ae
canibal <canaan@thetomato.co>
2020-04-16 03:08:50 -0400

    makes changeImage function operational hopefully (pre-testing)

--- final/changeImage.py
+++ final/changeImage.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
 import os
-import PIL
+import re
+from PIL import Image
 
+directory = os.path.join(os.getcwd(), "supplier-data/images/")
+file_list = [f for f in os.listdir(directory)) if not f.startswith('.')]
+for file in file_list:
+    im = Image.open(directory + file)
+    fixed = im.resize((600, 400))
+    fixed.convert("RGB").save(directory + file, "jpeg")
+    print("converted " + file)
