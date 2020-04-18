3a28a2d8567944f6f8cb9baa479b0d3580fc5b48
canibal <canaan@thetomato.co>
2020-04-18 06:37:02 -0400

    as functional as it can be without testing the email and other quirks on the "production" server

--- final/health_check.py
+++ final/health_check.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """Health Check.
 
 Checks for various system problems, and notifies the administrator to check
@@ -10,46 +11,70 @@ import emails
 import socket
 
 
-email_list = []
 sender = 'automation@example.com'
 recipient = '<username>@example.com'
 body = 'Please check your system and resolve the issue as soon as possible.'
+collector = {'sender': sender, 'recipient': recipient, 'subject': '', \
+             'body': body}
 
 
 def chk_cpu():
     """Check CPU."""
     cpu = psutil.cpu_percent()
-    assert (cpu < 80), 'high cpu alert!'
+    assert (cpu < 80), 'Error - CPU usage is over 80%'
     print("cpu looks good!")
 
 
 def chk_storage():
     """Check storage."""
     usage = psutil.disk_usage('/')
-    assert (usage[3] < 80), "low storage space!"
+    assert (usage[3] < 80), 'Error - Available disk space is less than 20%'
     print("disk usage is fine!")
 
 
 def chk_memory():
     """Check RAM."""
     memory = psutil.virtual_memory()
-    assert (memory[1] / 1024000 > 500), "excessive RAM usage!"
+    assert (memory[1] / 1024000 > 500), 'Error - Available memory \
+            is less than 500MB'
     print("sufficient available RAM!")
 
 
 def chk_server():
     """Check localhost resolves to loopback IP."""
     resolution = socket.gethostbyname('localhost')
-    assert (resolution != '127.0.0.1'), "check your '/etc/hosts' file!"
+    assert (resolution == '127.0.0.1'), 'Error - localhost cannot \
+            be resolved to 127.0.0.1'
     print("localhost resolves to correct IP!")
 
 
 def main():
     """Make it do what it do."""
-    chk_cpu()
-    chk_storage()
-    chk_memory()
-    chk_server()
+    email_list = []
+    try:
+        chk_cpu()
+    except AssertionError as error:
+        print('adding email to queue')
+        collector['subject'] = error.args[0]
+        email_list.append(list(collector.values()))
+    try:
+        chk_storage()
+    except AssertionError as error:
+        print('adding email to queue')
+        collector['subject'] = error.args[0]
+        email_list.append(list(collector.values()))
+    try:
+        chk_memory()
+    except AssertionError as error:
+        print('adding email to queue')
+        collector['subject'] = error.args[0]
+        email_list.append(list(collector.values()))
+    try:
+        chk_server()
+    except AssertionError as error:
+        print('adding email to queue')
+        collector['subject'] = error.args[0]
+        email_list.append(list(collector.values()))
     for message in email_list:
         emails.send_email(message)
 
